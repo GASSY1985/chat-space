df26fac5c6590919a2acefd681c4552c017aa451
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2019-12-19 21:26:22 +0900

    update:1219_2130

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,16 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image == null){
+      var html = $(html).append(`</li>`)
+    } else {
+      var html = $$(html).append(`<div class = 'hoge'><img src = '${data.image.url}' width="256" height="256"}</div></li>`)
+    }
+    return html;
+  }
   $('.new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new formData(message);
-    var url = $(message).attr('action')
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: 'POST',
@@ -10,6 +18,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
     })
   });
 });
