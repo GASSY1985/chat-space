2641c644012cc4313d6ffabd737122919a553d70
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2019-12-25 21:59:20 +0900

    update:1225_2200

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -50,4 +50,28 @@ $('.new_message').on('submit', function(e){
     $('.submit').prop('disabled', false);
   })
 });
-});
+  var reloadMessages = function(){  
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insertHTML = '';
+        $.each(messages, function(i,message){
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.chat-room').animate({ scrollTop: $('.chat-room')[0].scrollHeight });
+        $("#new_message")[0].reset();
+        $('.submit').prop("disabled", false);
+      }
+    })
+    }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+  };
+})
